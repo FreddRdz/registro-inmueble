edf18f92c01263708a3eeeeb75c02bbd219abc04
Alfredo Rodriguez <fredd.073100@gmail.com>
2022-11-04 11:02:40 -0600

    Agregando comentarios al archivo de javascript

--- js/script.js
+++ js/script.js
@@ -1,41 +1,19 @@
-// const selectEntregaInput = document.querySelector('.select-entrega');
-// const selectAnioInput = document.querySelector('.select-anio');
-
-// selectEntregaInput.addEventListener('change', () => {
-//   if (selectEntregaInput.value === 'Entrega inmediata') {
-//     selectAnioInput.disabled = true;
-//   } else {
-//     selectAnioInput.disabled = false;
-//   }
-// });
-
-// const input = document.querySelector('#proyectoEstanciasToronto');
-// console.log(input);
-
-// input.addEventListener('change', () => {
-//   console.log(input.checked);
-// });
-
+// Variables de la sección de desarrollador
 const newDeveloperContainer = document.querySelector('.nuevo-desarrollador');
 const developerInfoContainer = document.querySelector('.info-desarrollador');
 const selectDeveloper = document.querySelector('.select-desarrollador');
 const kindOfProject = document.querySelector('.container-proyecto');
 
-// console.log(selectDeveloper);
-
 selectDeveloper.addEventListener('change', () => {
   if (selectDeveloper.value === 'Selecciona...') {
-    newDeveloperContainer.classList.add('d-none');
-    developerInfoContainer.classList.add('d-none');
-    kindOfProject.classList.add('d-none');
+    // Cerrar toda la sección de desarrpllador
+    handleCloseDeveloperContainer();
   } else if (selectDeveloper.value === 'Desarrollador nuevo') {
-    newDeveloperContainer.classList.toggle('d-none');
-    developerInfoContainer.classList.add('d-none');
-    kindOfProject.classList.add('d-none');
+    // Abrir sección de nuevo desarrollador para registrarlo
+    handleOpenNewDeveloperContainer();
   } else {
-    newDeveloperContainer.classList.add('d-none');
-    developerInfoContainer.classList.toggle('d-none');
-    kindOfProject.classList.toggle('d-none');
+    // Abrir sección de un desarrollador ya existente
+    handleOpenDeveloperContainer();
   }
 });
 
@@ -140,51 +118,63 @@ selectProjectRegister.addEventListener('change', () => {
   }
 });
 
+// CheckRadio de las terrazas en sección casas si es verdadero
 const checkRadioTerrazaM2CasasTrue = document.querySelector(
   '#flexRadioWalkinTerrazaTrueCasas'
 );
 
+// CheckRadio de las terrazas en sección casas si es falso
 const checkRadioTerrazaM2CasasFalse = document.querySelector(
   '#flexRadioWalkinTerrazaFalseCasas'
 );
 
+// Input del campo de los metros cuadrados de las terrazas en sección casas
 const inputTerrazaM2Casas = document.querySelector(
   '#inputNuevoProyectoM2DeTerrazasCasas'
 );
 
 checkRadioTerrazaM2CasasTrue.addEventListener('change', () => {
+  // Si el checkbox es sí, habilitaremos el campo
   if (checkRadioTerrazaM2CasasTrue.checked === true) {
     inputTerrazaM2Casas.disabled = false;
   }
 });
 
 checkRadioTerrazaM2CasasFalse.addEventListener('change', () => {
+  // Si el checkbox es no, deshabilitaremos el campo y vaciaremos el valor del input
   if (checkRadioTerrazaM2CasasTrue.checked === false) {
     inputTerrazaM2Casas.disabled = true;
+    inputTerrazaM2Casas.value = '';
   }
 });
 
+// CheckRadio de las terrazas en sección departamentos si es verdadero
 const checkRadioTerrazaM2DepartamentosTrue = document.querySelector(
   '#flexRadioWalkinTerrazaTrueDepartamentos'
 );
 
+// CheckRadio de las terrazas en sección departamentos si es falso
 const checkRadioTerrazaM2DepartamentosFalse = document.querySelector(
   '#flexRadioWalkinTerrazaFalseDepartamentos'
 );
 
+// Input del campo de los metros cuadrados de las terrazas en sección departamentos
 const inputTerrazaM2Departamentos = document.querySelector(
   '#inputNuevoProyectoM2DeTerrazasDepartamentos'
 );
 
 checkRadioTerrazaM2DepartamentosTrue.addEventListener('change', () => {
+  // Si el checkbox es sí, habilitaremos el campo
   if (checkRadioTerrazaM2DepartamentosTrue.checked === true) {
     inputTerrazaM2Departamentos.disabled = false;
   }
 });
 
 checkRadioTerrazaM2DepartamentosFalse.addEventListener('change', () => {
+  // Si el checkbox es no, deshabilitaremos el campo y vaciaremos el valor del input
   if (checkRadioTerrazaM2DepartamentosTrue.checked === false) {
     inputTerrazaM2Departamentos.disabled = true;
+    inputTerrazaM2Departamentos.value = '';
   }
 });
 
@@ -222,4 +212,22 @@ const handleSelectTerrenosInputs = () => {
   formContainerCaracteristicasCasas.classList.add('d-none');
   formContainerCaracteristicasDepartamentos.classList.add('d-none');
   formContainerCaracteristicasTerrenos.classList.remove('d-none');
+};
+
+const handleCloseDeveloperContainer = () => {
+  newDeveloperContainer.classList.add('d-none');
+  developerInfoContainer.classList.add('d-none');
+  kindOfProject.classList.add('d-none');
+};
+
+const handleOpenNewDeveloperContainer = () => {
+  newDeveloperContainer.classList.toggle('d-none');
+  developerInfoContainer.classList.add('d-none');
+  kindOfProject.classList.add('d-none');
+};
+
+const handleOpenDeveloperContainer = () => {
+  newDeveloperContainer.classList.add('d-none');
+  developerInfoContainer.classList.toggle('d-none');
+  kindOfProject.classList.toggle('d-none');
 };
